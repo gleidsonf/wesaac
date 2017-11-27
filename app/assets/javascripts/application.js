49359cda0a2e08eabe20da7c6dc2615454254d01
Gleidson Silva <gleidson.fgs@gmail.com>
2017-11-27 10:38:15 -0300

    refatorando codigo html para rails

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,47 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require rails-ujs
+//= require vendor/modernizr-2.8.3-respond-1.4.2.min
+//= require vendor/jquery-library
+//= require vendor/bootstrap.min
+//= require jquery.singlePageNav.min
+//= require photoswipe-ui-default
+//= require customScrollbar.min
+//= require owl.carousel.min
+//= require prettyPhoto
+//= require tilt.jquery
+//= require countdown
+//= require parallax
+//= require gmap3
+//= require main
+//= require turbolinks
+//= require_tree .
+//
+//
+// <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+//
+//
+// <script src="js/vendor/jquery-library.js"></script>
+// <script src="js/vendor/bootstrap.min.js"></script>
+// <script src="https://maps.google.com/maps/api/js?key=AIzaSyCR-KEWAVCn52mSdeVeTqZjtqbmVJyfSus&amp;language=en"></script>
+// <script src="js/jquery.singlePageNav.min.js"></script>
+// <script src="js/photoswipe-ui-default.js"></script>
+// <script src="js/customScrollbar.min.js"></script>
+// <script src="js/owl.carousel.min.js"></script>
+// <script src="js/photoswipe.min.js"></script>
+// <script src="js/prettyPhoto.js"></script>
+// <script src="js/tilt.jquery.js"></script>
+// <script src="js/countdown.js"></script>
+// <script src="js/parallax.js"></script>
+// <script src="js/gmap3.js"></script>
+// <script src="js/main.js"></script>
---
+++ app/assets/javascripts/nomes.js
@@ -0,0 +1,244 @@
+steering_committee = [{
+    nome: 'Anarosa Alves Franco Brandão',
+    universidade: 'USP,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Diana Francisca Adamatti',
+    universidade: 'FURG,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Rejane Frozza',
+    universidade: 'UNISC,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Gustavo Alberto Giménez Lugo',
+    universidade: 'UTFPR,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'João Luis Tavares da Silva',
+    universidade: 'UCS,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Jomi Fred Hubner',
+    universidade: 'UFSC,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Rafael Heitor Bordini',
+    universidade: 'PUCRS,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Ricardo Choren',
+    universidade: 'IME,',
+    pais: 'Brasil'
+  },
+  {
+    nome: 'Viviane Torres da Silva',
+    universidade: 'UFF,',
+    pais: 'Brasil'
+  }
+]
+
+program_committe = [{
+  "nome": "Adamatti, Diana",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "Aguiar, Marilton Sanchotene de",
+  "universidade": "UFPel",
+  "pais": "Brasil"
+}, {
+  "nome": "Alencar, Fernanda",
+  "universidade": "UFPE",
+  "pais": "Brasil"
+}, {
+  "nome": "Araújo, Ricardo",
+  "universidade": "UFPel",
+  "pais": "Brasil"
+}, {
+  "nome": "Bajo Pérez, Javier",
+  "universidade": "USAL",
+  "pais": "Espanha"
+}, {
+  "nome": "Barbosa, Raquel",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "Bazzan, Ana L. C.",
+  "universidade": "UFRGS",
+  "pais": "Brasil"
+}, {
+  "nome": "Billa, Cleo",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "Boissier, Olivier",
+  "universidade": "EMSE",
+  "pais": "França"
+}, {
+  "nome": "Bordini, Rafael Heitor",
+  "universidade": "PUCRS",
+  "pais": "Brasil"
+}, {
+  "nome": "Campos, André",
+  "universidade": "UFRN",
+  "pais": "Brasil"
+}, {
+  "nome": "Carine Webber",
+  "universidade": "UCS,",
+  "pais": "Brasil"
+}, {
+  "nome": "Casare, Sara",
+  "universidade": "IBM",
+  "pais": "Brasil"
+}, {
+  "nome": "Castro, Paulo André L.",
+  "universidade": "ITA",
+  "pais": "Brasil"
+}, {
+  "nome": "Choren, Ricardo",
+  "universidade": "IME/RJ",
+  "pais": "Brasil"
+}, {
+  "nome": "Cortés, Mariela Inés",
+  "universidade": "UECE",
+  "pais": "Brasil"
+}, {
+  "nome": "Costa, Antonio Carlos Rocha",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "Costa, Evandro ",
+  "universidade": "UFAL",
+  "pais": "Brasil"
+}, {
+  "nome": "Coutinho, Luciano",
+  "universidade": "UFMA",
+  "pais": "Brasil"
+}, {
+  "nome": "David, Nuno",
+  "universidade": "ISCTE",
+  "pais": "Portugal"
+}, {
+  "nome": "Dimuro, Graçaliz",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "de Paz Santana, Juan Francisco",
+  "universidade": "USAL",
+  "pais": "Espanha"
+}, {
+  "nome": "Ferreira Jr., Paulo R.",
+  "universidade": "UFPel",
+  "pais": "Brasil"
+}, {
+  "nome": "Frozza, Rejane",
+  "universidade": "UNISC",
+  "pais": "Brasil"
+}, {
+  "nome": "Gatti, Maira",
+  "universidade": "IBM",
+  "pais": "Brasil"
+}, {
+  "nome": "Giménez-Lugo, Gustavo",
+  "universidade": "UTFPR",
+  "pais": "Brasil"
+}, {
+  "nome": "Gonçalves, Eder",
+  "universidade": "FURG",
+  "pais": "Brasil"
+}, {
+  "nome": "Hubner, Jomi Fred",
+  "universidade": "UFSC",
+  "pais": "Brasil"
+}, {
+  "nome": "Koch, Fernando",
+  "universidade": "IBM",
+  "pais": "Research Brasil"
+}, {
+  "nome": "Leite, João",
+  "universidade": "Portugal",
+  "pais": ""
+}, {
+  "nome": "La Prieta Pintado",
+  "universidade": "USAL",
+  "pais": "Espanha"
+}, {
+  "nome": "Lemke, Ana Paula",
+  "universidade": "IFRS",
+  "pais": "Brasil"
+}, {
+  "nome": "Lorenzi, Fabiana",
+  "universidade": "UFRGS",
+  "pais": "Brasil"
+}, {
+  "nome": "Marchi, Jerusa",
+  "universidade": "UFSC",
+  "pais": "Brasil"
+}, {
+  "nome": "Moreira, Alvaro",
+  "universidade": "UFRGS",
+  "pais": "Brasil"
+}, {
+  "nome": "Nardin, Luis Gustavo",
+  "universidade": "University",
+  "pais": "of Idaho USA"
+}, {
+  "nome": "Nunes, Ingrid",
+  "universidade": "UFRGS",
+  "pais": "Brasil"
+}, {
+  "nome": "Okuyama, Fabio",
+  "universidade": "IFRS",
+  "pais": "Brasil"
+}, {
+  "nome": "Ricci, Alessandro",
+  "universidade": "University",
+  "pais": "of Bologna Italia"
+}, {
+  "nome": "Rosa, Paulo",
+  "universidade": "IME",
+  "pais": "Brasil"
+}, {
+  "nome": "Sichman, Jaime Simão",
+  "universidade": "USP",
+  "pais": "Brasil"
+}, {
+  "nome": "Silva, Joao Luis",
+  "universidade": "UCS",
+  "pais": "Brasil"
+}, {
+  "nome": "Silva, Viviane",
+  "universidade": "UFF",
+  "pais": "Brasil"
+}, {
+  "nome": "Simari, Guillermo Ricardo",
+  "universidade": "Universidad",
+  "pais": "Nacional del Sur Argentina"
+}, {
+  "nome": "Tacla, Cesar A.",
+  "universidade": "UTFPR",
+  "pais": "Brasil"
+}, {
+  "nome": "Tedesco, Patricia",
+  "universidade": "UFPE",
+  "pais": "Brasil"
+}, {
+  "nome": "Trigo, Paulo",
+  "universidade": "ISEL",
+  "pais": "Portugal"
+}, {
+  "nome": "Vasconcelos, Wamberto",
+  "universidade": "University",
+  "pais": "of Aberdeen UK"
+}, {
+  "nome": "Webber, Carine",
+  "universidade": "UCS",
+  "pais": "Brasil"
+}]
