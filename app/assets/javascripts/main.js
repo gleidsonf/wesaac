40d55d57e25ff783a89dc348f35f690895d08560
Gleidson Silva <gleidson.fgs@gmail.com>
2017-11-29 13:14:53 -0300

    asdiaj

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -162,9 +162,9 @@ jQuery(document).on('ready', function() {
 	var _tg_locationmap = jQuery("#tg-locationmap");
 	_tg_locationmap.gmap3({
 		marker: {
-			address: "Avenida Washington Soares, 1321 - Reitoria - Edson Queiroz, Fortaleza - CE, 60811-905",
+			address: "1600 Elizabeth St, Melbourne, Victoria, Australia",
 			options: {
-				title: "Wesaac 2018",
+				title: "Event Orgnizer",
 				icon: "images/map-marker.png",
 			}
 		},
