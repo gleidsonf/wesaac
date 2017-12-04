a5651110ba86a3a6678bbf09de7032574b0763bb
Gleidson Silva <gleidson.fgs@gmail.com>
2017-12-04 10:05:36 -0300

    error pages

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   root "home#index"
   get "/committees" => "home#committees"
   get "/info" => "home#info"
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
 end
